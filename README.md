f25604ff8de4b3fd8ff928c37a4f31b278c24497
squeek <squeek502@hotmail.com>
2015-03-19 21:44:35 -0700

    Add instructions to setting up Hammer for Steam FF  * Closes #1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fortressforever-maps
 The map source files of Fortress Forever
+
+## Setting up Hammer
+
+1. **Install Source SDK**
+  * In Steam, go to the Library tab and select *Tools* from the dropdown
+  * Find Source SDK in the list and install it
+2. **Setup Source SDK**
+  * Run Source SDK once to install it and set the `sourcesdk` environment variable. Close it once it is finished.
+  * Run `setup.bat`
+3. **Launch Hammer**
+  * Open Source SDK
+  * Select *Source Engine 2006* as the *Engine Version* and *Fortress Forever* as the *Current Game*
+  * Double click *Hammer Editor* under the *Applications* heading
---
+++ setup.bat
@@ -0,0 +1,85 @@
+@echo off
+FOR /f "tokens=1,2*" %%E in ('reg query "HKEY_CURRENT_USER\Software\Valve\Steam"') DO (
+	IF "%%E"=="SteamPath" (
+		set SteamPath=%%G
+	)
+)
+IF "%SteamPath%"=="" (
+	echo Not able to determine Steam install path. Make sure you have Steam installed
+	pause
+	exit /B
+)
+IF "%sourcesdk%"=="" (
+	echo SourceSDK environment variable is not set. Run Source SDK once before executing this script
+	pause
+	exit /B
+)
+
+set GameDir=%SteamPath%/steamapps/common/Fortress Forever
+set ModDir=%GameDir%/FortressForever
+
+IF NOT EXIST "%GameDir%" (
+	echo "%GameDir%" does not exist. Make sure Fortress Forever is installed
+	pause
+	exit /B
+)
+
+set Ep1BinDir=%sourcesdk%\bin\ep1\bin
+set ConfigFile=%Ep1BinDir%\GameConfig.txt
+set SchemeDir=%Ep1BinDir%\resource
+set SchemeFile=%SchemeDir%\SourceScheme.res
+
+echo(
+echo Writing "%ConfigFile%"...
+(
+echo "Configs"
+echo {
+echo 	"SDKVersion"		"2"
+echo 	"Games"
+echo 	{
+echo 		"Fortress Forever"
+echo 		{
+echo 			"GameDir"		"%ModDir%"
+echo 			"hammer"
+echo 			{
+echo 				"GameData0"		"%ModDir%/fortressforever.fgd"
+echo 				"TextureFormat"		"5"
+echo 				"MapFormat"		"4"
+echo 				"DefaultTextureScale"		"0.250000"
+echo 				"DefaultLightmapScale"		"16"
+echo 				"GameExe"		"%GameDir%/hl2.exe"
+echo 				"DefaultSolidEntity"		"func_detail"
+echo 				"DefaultPointEntity"		"info_ff_script"
+echo 				"BSP"		"%Ep1BinDir%\vbsp.exe"
+echo 				"Vis"		"%Ep1BinDir%\vvis.exe"
+echo 				"Light"		"%Ep1BinDir%\vrad.exe"
+echo 				"GameExeDir"		"%GameDir%/Fortress Forever"
+echo 				"MapDir"		"%ModDir%/mapsrc"
+echo 				"BSPDir"		"%ModDir%/maps"
+echo 				"CordonTexture"		"tools\toolsskybox"
+echo 				"MaterialExcludeCount"		"0"
+echo 			}
+echo 		}
+echo 	}
+echo }
+) >"%ConfigFile%"
+echo  -^> Done
+
+:: Need a SourceScheme.res in bin/ep1/bin/resource so that Hammer doesn't throw
+:: a 'Failed to load the default scheme file' error
+:: The contents of this scheme file are unimportant; it can be totally blank
+echo(
+echo Writing "%SchemeFile%"...
+IF NOT EXIST "%SchemeFile%" (
+	IF NOT EXIST "%SchemeDir%" mkdir "%SchemeDir%"
+    echo( >"%SchemeFile%"
+    echo  -^> Done
+) ELSE (
+    echo  -^> File already exists
+)
+
+echo(
+echo Source SDK setup completed successfully.
+echo(
+
+pause
